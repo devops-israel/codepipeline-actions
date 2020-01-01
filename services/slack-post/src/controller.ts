792e81582e9542deb1fb809e7e5b11ea0a9830ce
Meir Gabay <unfor19@gmail.com>
2020-01-01 15:57:51 +0200

    ADD - codebuild metadata file

--- services/slack-post/src/controller.ts
+++ services/slack-post/src/controller.ts
@@ -4,23 +4,19 @@ const slack_bot_oauth_token: string = process.env.SLACK_BOT_OAUTH_TOKEN;
 
 export async function sendSlack(event: any) {
   console.log(JSON.stringify(event));
-  if (event['Records']) {
-    console.log('Post approval info');
-    const post_body = await getTemplate(event);
-    const headers = {
-      'Content-Type': 'application/json; charset=utf-8',
-      Authorization: `Bearer ${slack_bot_oauth_token}`,
-    };
+  console.log('Post info to slack channel');
+  const post_body = await getTemplate(event);
+  const headers = {
+    'Content-Type': 'application/json; charset=utf-8',
+    Authorization: `Bearer ${slack_bot_oauth_token}`,
+  };
 
-    return promisePost('https://slack.com/api/chat.postMessage', post_body, headers)
-      .then(function(postResponse: any) {
-        return postResponse.data;
-      })
-      .catch(function(err: any) {
-        console.log(err);
-        return err;
-      });
-  } else {
-    return 'Not SNS Event';
-  }
+  return promisePost('https://slack.com/api/chat.postMessage', post_body, headers)
+    .then(function(postResponse: any) {
+      return postResponse.data;
+    })
+    .catch(function(err: any) {
+      console.log(err);
+      return err;
+    });
 }
