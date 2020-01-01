51da7dde65b0f58bc1f0ff784a9c25b56567a8a0
Meir Gabay <unfor19@gmail.com>
2020-01-02 00:50:16 +0200

    MOD - shared invoke lambda function

--- services/codepipeline-shared/src/controller.ts
+++ services/codepipeline-shared/src/controller.ts
@@ -1,7 +1,29 @@
+const Lambda = require('aws-sdk/clients/lambda');
+const lambda = new Lambda({ apiVersion: '2015-03-31' });
 import { BaseS3Object } from '../../shared/src/model';
 
 export async function removeLatestVersion(bucketname: string, filename: string) {
-    return new BaseS3Object(bucketname, filename, null, filename).deleteObjectLatestVersion();
+  return new BaseS3Object(bucketname, filename, null, filename).deleteObjectLatestVersion();
 }
 
 export { SnsEvent } from '../../shared/src/interfaces';
+
+export async function invoke_lambda(function_name: any, payload: any) {
+  return new Promise(function(resolve, reject) {
+    var params = {
+      FunctionName: function_name,
+      Payload: JSON.stringify(payload),
+      InvocationType: 'Event', // async
+    };
+    console.log(`Invoking Lambda Function: ${function_name}`);
+    lambda.invoke(params, function(err: any, data: any) {
+      if (err) {
+        console.log(err, err.stack);
+        reject(err);
+      } else {
+        console.log(data);
+        resolve(data);
+      }
+    });
+  });
+}
