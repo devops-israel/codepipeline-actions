13d9522f470867ce1b6c022f879789cae7906ed2
Meir Gabay <unfor19@gmail.com>
2020-01-02 10:59:07 +0200

    MOD - slack shared - prettier

--- services/slack-shared/src/controller.ts
+++ services/slack-shared/src/controller.ts
@@ -1,40 +1,40 @@
 const axios = require('axios');
 export async function promisePost(url: string, post_body: any, headers: any) {
-    return new Promise(function(resolve, reject) {
-        const options = {
-            method: 'POST',
-            headers: headers,
-            data: post_body,
-            url,
-        };
-        axios(options)
-            .then(function(response: any) {
-                resolve(response);
-            })
-            .catch(function(err: any) {
-                reject(err);
-            });
-    });
+  return new Promise(function(resolve, reject) {
+    const options = {
+      method: 'POST',
+      headers: headers,
+      data: post_body,
+      url,
+    };
+    axios(options)
+      .then(function(response: any) {
+        resolve(response);
+      })
+      .catch(function(err: any) {
+        reject(err);
+      });
+  });
 }
 
 export async function promiseGet(url: string, headers: any) {
-    return new Promise(function(resolve, reject) {
-        const options = {
-            method: 'GET',
-            headers: headers,
-            url,
-        };
-        axios(options)
-            .then(function(response: any) {
-                resolve(response);
-            })
-            .catch(function(err: any) {
-                reject(err);
-            });
-    });
+  return new Promise(function(resolve, reject) {
+    const options = {
+      method: 'GET',
+      headers: headers,
+      url,
+    };
+    axios(options)
+      .then(function(response: any) {
+        resolve(response);
+      })
+      .catch(function(err: any) {
+        reject(err);
+      });
+  });
 }
 export function toTitleCase(str: string) {
-    return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
+  return str.replace(/\w\S*/g, function(txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
 }
