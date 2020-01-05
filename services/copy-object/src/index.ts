ed4f865ced662293f379ec5c5be7ce5f18e2cc0d
Meir Gabay <unfor19@gmail.com>
2020-01-05 18:28:23 +0200

    MOD - copy object supports env vars

--- services/copy-object/src/index.ts
+++ services/copy-object/src/index.ts
@@ -1,9 +1,9 @@
 import { invokeLambda, InvokeLambdaEvent } from './controller';
 exports.handler = async (event: InvokeLambdaEvent, context: any, _callback: any) => {
-    const job_id = JSON.stringify(event['CodePipeline.job'].id);
-    console.log(`
+  const job_id = JSON.stringify(event['CodePipeline.job'].id);
+  console.log(`
     Approve: aws codepipeline put-job-success-result --job-id ${job_id}
     Reject:  aws codepipeline put-job-failure-result --job-id ${job_id}`);
-    const response = await invokeLambda(event, context);
-    return response;
+  const response = await invokeLambda(event, context);
+  return response;
 };
